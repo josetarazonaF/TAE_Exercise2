7f0a8cc2f93d856948eb7823e5e124e30537d591
Jose Tarazona <jose.tarazona@globant.com>
2022-04-22 15:55:39 -0500

    finished exercise 2

---
+++ src/test/FacebookPage.java
@@ -0,0 +1,21 @@
+package test;
+
+public class FacebookPage {
+
+    //elements for login facebook page
+    String emailField = "By.id(#email)";
+    String passwordField = "By.id(#pass)";
+    String loginButton = "By.id(#u_0_d_SL)";
+
+    //elements for logout facebook page
+    String logoutButton = "By.css(div.a8nywdso.sj5x9vvc.rz4wbd8a.ecm0bbzt > div > div:nth-child(4) > div)";
+
+    //elements for my profile facebook page
+    String profileButton = "By.css(.bp9cbjyn a[href='/me/'])";
+
+    //elements for my friends facebook page
+    String friendsButton = "By.css(div.soycq5t1.l9j0dhe7 div:nth-child(1) a:nth-child(4))";
+
+
+
+}
---
+++ src/test/TestPagePaul.java
@@ -0,0 +1,16 @@
+package test;
+
+public class TestPagePaul {
+
+    String elementTagJukka = "By.css(div.container:nth-child(1) address:nth-child(6) > a:nth-child(1))";
+    String elementListThree = "By.css(div.container:nth-child(1) ul:nth-child(8) > li:nth-child(3))";
+    String elementMenuOne = "By.css(div.container:nth-child(1) menu:nth-child(10) > li:nth-child(1))";
+    String elementTaJukka = "By.css(body:nth-child(2) div.container:nth-child(1) address:nth-child(6) > a:nth-child(1))";
+    String elementButtonCool = "By.id(#but)";
+    String elementButtonReset2 = "By.id(#f01)";
+    String elementSelectThree = "By.css(div:nth-child(11) select:nth-child(3) > option:nth-child(3))";
+    String elementTable100k = "By.css(table:nth-child(18) tbody:nth-child(2) tr:nth-child(4) > td:nth-child(3))";
+    String elementHyphenationText = "By.css(div.container:nth-child(1) > p:nth-child(23))";
+    String elementJavascriptTitle = "By.css(div.container:nth-child(1) > h3:nth-child(26))";
+
+}
